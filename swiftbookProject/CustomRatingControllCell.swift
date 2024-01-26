4151730ede8cb1d6a196beb13e73fdfc28e34558
Vanoaleksa <vanoaleksa111@gmail.com>
2024-01-26 18:05:23 +0300

    Adding custom rating controll and fix saving editing place

---
+++ swiftbookProject/CustomRatingControllCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomRatingControllCell.swift
+//  swiftbookProject
+//
+//  Created by MacBook on 26.01.24.
+//
+
+import UIKit
+
+class CustomRatingControllCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
